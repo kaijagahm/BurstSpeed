0bbdb2fb5e586c41b4b42eeac7b1e075d7fc9887
Kaija Gahm <kaija.gahm@gmail.com>
2020-02-07 10:28:48 -0500

    added car and rationales for each package

--- libraries.R
+++ libraries.R
@@ -1,21 +1,22 @@
 # Libraries to load
 library(matrixStats)
-library(tidyverse)
+library(tidyverse) # because of course
 library(bootstrap)
 library(pracma)
-library(stringr)
-library(trajr)
+library(stringr) # for tadpole names
+library(trajr) # to make trajectories from coordinates
 library(data.table)
-library(lubridate)
+library(lubridate) 
 library(smooth)
 library(zoo)
-library(magrittr)
+library(magrittr) # for pipes
 library(DAAG)
-library(viridis)
-library(geomorph)
-library(abind)
-source("https://raw.githubusercontent.com/kaijagahm/general/master/locate.nas.txt")
-library(lme4)
-library(GGally)
-library(lmerTest)
-library(MuMIn)
+library(viridis) # for pretty plot colors
+library(geomorph) # for geometric morphometrics
+library(abind) # to bind together arrays of landmarks
+source("https://raw.githubusercontent.com/kaijagahm/general/master/locate.nas.txt") # to find NA's in data
+library(lme4) # for lmer models
+library(GGally) # for ggpairs
+library(lmerTest) # to get quasi-p-values on lmer models
+library(MuMIn) # for dredge
+library(car) # for the vif function
