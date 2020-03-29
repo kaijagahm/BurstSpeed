7a521fcfd526eee3348f5d5b371d422f6b912107
Kaija Gahm <kaija.gahm@gmail.com>
2020-03-28 21:49:19 -0400

    moved reorder_within and scale_x_reordered to the helper funs script

--- plot_helper_funs.R
+++ plot_helper_funs.R
@@ -54,3 +54,14 @@ shift_legend <- function(p){
   
   return(gp)
 }
+
+
+reorder_within <- function(x, by, within, fun = mean, sep = "___", ...) {
+  new_x <- paste(x, within, sep = sep)
+  stats::reorder(new_x, by, FUN = fun)
+}
+
+scale_x_reordered <- function(..., sep = "___") {
+  reg <- paste0(sep, ".+$")
+  ggplot2::scale_x_discrete(labels = function(x) gsub(reg, "", x), ...)
+}
